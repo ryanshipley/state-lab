adce46f43f01c69a2c3d4c371ea13201065b07b9
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-21 13:08:52 -0400

    renders selected photo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, {useState} from "react";
 import './App.css';
+import imagesArr from './ImageData';
 
 function App() {
+const [bigImage, setBigImage] = useState(imagesArr[0].img);
+
+const handleClick = (imgUrl) =>{setBigImage(imgUrl)};
+
+const images = imagesArr.map((image, index)=>{
+  return(
+    <img className="thumb"
+    src={image.img}
+    alt={image.city}
+    key={index}
+    onClick={() =>{handleClick(image.img)}}
+    />
+  );
+});
+
   return (
     <div className="App">
-<h1>hello world</h1>
+      <h1>Cities of the World</h1>
+      <div className="wrapper">
+        <div className="thumbnails">
+        {images}
+      </div>
+        {<img id="bigimage" src={bigImage}/>}
+      </div>
 </div>
   );
-}
+};
 
 export default App;
